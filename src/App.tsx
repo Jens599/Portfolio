cd636475ea7f309215afe316ba42b2afa2a3e595
Jens599 <workjenss@gmail.com>
2024-08-25 21:44:55 +0545

    Github pages test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const app = () => {
-  return <>App Working ?? </>
+  return (
+    <>
+      <div className="bg-red-500 flex justify-center mt-10 text-5xl py-8">
+        App Working ??
+      </div>
+    </>
+  )
 }
 
 export default app
