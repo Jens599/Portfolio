19e124ebaafdc30903486725bab9b49cdfa26511
Jens599 <workjenss@gmail.com>
2024-08-28 13:44:06 +0545

    Hero Section v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        solid: '-12px 12px 0px 0px var(--tw-shadow-color)',
+      },
+      colors: {
+        royal: {
+          400: '#4169E1',
+          500: '#365AC3',
+          600: '#2B4AA4',
+          700: '#203A85',
+          800: '#152A66',
+        },
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
+        },
+      },
+      animation: {
+        blink: 'blink 1000ms ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 }
